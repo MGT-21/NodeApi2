476ae89f885164afa333a2bf5c8d80ce82cf1f3f
MGT-21 <mgt21inv@gmail.com>
2026-03-11 16:57:08 -0300

    :sparkles: feat: create environment variables

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'Knex'
+
+if (!process.env.DATABASE_URL){
+  throw new Error('Database_URL env not found')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
