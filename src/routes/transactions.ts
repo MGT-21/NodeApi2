2ea5e51f3e9f2a3d8b470cc9e5e01daafb6f864d
MGT-21 <mgt21inv@gmail.com>
2026-03-21 00:17:59 -0300

    :sparkles: feat: Fastify Plugins

---
+++ src/routes/transactions.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transaction = await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title: 'Test transaction',
+      amount: 1000,
+    }).returning('*')
+
+    return transaction
+  })
+}
